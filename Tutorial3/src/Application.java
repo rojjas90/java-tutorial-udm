92d396c8b1ab7428530dd69968efafb3e005891a
grojas <grojas@dwsoftware.mx>
2016-11-15 16:02:06 -0600

    Se agrego tutorial 3 y se realizaron ejemplos con la clase Strign en el archivo Application

---
+++ Tutorial3/src/Application.java
@@ -0,0 +1,21 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 7;
+
+		String text = "Hello";
+		String blank = "  ";
+		String name = "Rojas";
+		String greeting = text + blank + name;
+
+		System.out.println(greeting);
+
+		System.out.println("Hello " + name);
+
+		System.out.println("My integer is: " + myInt);
+
+		double myDouble = 4.6;
+
+		System.out.println("My double is:" + myDouble);
+	}
+}
