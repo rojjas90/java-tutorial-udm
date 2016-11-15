f46367754bd5c0ee10d60b1fa5cebdaef7325f4a
Rojas <sajor90@gmail.com>
2016-11-15 14:58:45 -0600

    Se agregaron tipos de datos en clase Application

--- Tutorial1/src/Application.java
+++ Tutorial1/src/Application.java
@@ -1,7 +1,31 @@
 
 public class Application {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		// int numbers
+		int myNumber = 23;
+		short myShort = 244;
+		long myLong = 234235;
+		
+		//float numbers
+		double myDouble = 23.23;
+		float myFloat = 234.4f;
+		
+		char myChar = 't';
+		boolean myBoolean = true;
+		
+		byte myByte = 23;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		
+		System.out.println(myByte);
 	}
 
 }
